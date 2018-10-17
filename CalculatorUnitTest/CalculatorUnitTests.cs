f09afcdf5e3942e9de117c01aa667dc612df0845
Taha Bhat <tahabhat47@hotmail.com>
2018-10-17 12:14:14 +0530

    added unit test

---
+++ CalculatorUnitTest/CalculatorUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class CalculatorUnitTests
+    {
+        ScientificCalculator calc = new Calculator.ScientificCalculator();
+        [TestMethod]
+        public void AddTest()
+        {
+
+            Assert.AreEqual(7, calc.Add(3, 4));
+        }
+        [TestMethod]
+        public void SubtractionTest()
+        {
+
+            Assert.AreEqual(-1, calc.Subtraction(3, 4));
+        }
+        [TestMethod]
+        public void MultiplyTest()
+        {
+
+            Assert.AreEqual(12, calc.Multiply(3, 4));
+        }
+        [TestMethod]
+        public void Division()
+        {
+
+            Assert.AreEqual(7.0, calc.Division(28, 4));
+        }
+        [TestMethod]
+        public void Percentile()
+        {
+
+            Assert.AreEqual(33, calc.Percentile(33, 100));
+        }
+    }
+}
+
